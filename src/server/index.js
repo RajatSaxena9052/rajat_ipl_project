3f38651ad450bf8174ed2900892567e3aadfbde8
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-05-18 15:46:25 +0530

    Added fs module to dump the result json data into the output folder in json fromat

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,11 @@
 const csv = require("csvtojson");
+const fs = require('fs');
 const matchesFilePath = "../data/matches.csv"
 const deliveriesFilePath = "../data/deliveries.csv"
 const matchesPlayedPerYear = require("./matchesPlayedPerYear")
+const matchesWonPerTeamPerYear = require("./matchesWonPerTeamPerYear");
+
+const path1 = "../public/output/matchesPerYear.json"
 
 function main() {
     csv()
@@ -10,9 +14,23 @@ function main() {
             csv()
                 .fromFile(deliveriesFilePath)
                 .then((deliveries) => {
-                    console.log(matchesPlayedPerYear(matches))
+                    let result1 = matchesPlayedPerYear(matches)
+                    let result2 = matchesWonPerTeamPerYear(matches)
+                    console.log(result2)
+                    convertAndSave(result1)
                 })
         })
 }
-
+function convertAndSave(data) {
+    data = JSON.stringify(data)
+    fs.writeFile(path1, data, 'utf8' , (e) => {
+        if(e){
+            return e
+        }
+    })/*.writeFile(path1, data, 'utf8' , (e) => {
+        if(e){
+            return e
+        }
+    })*/
+}
 main()
