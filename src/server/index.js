884ba5d19823ba54da729d102dbefd516a2adde9
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-05-18 19:35:35 +0530

    updated the indentation

--- src/server/index.js
+++ src/server/index.js
@@ -4,9 +4,13 @@ const matchesFilePath = "../data/matches.csv"
 const deliveriesFilePath = "../data/deliveries.csv"
 const matchesPlayedPerYear = require("./matchesPlayedPerYear")
 const matchesWonPerTeamPerYear = require("./matchesWonPerTeamPerYear");
+const extraRunsConcededPerTeam2016 = require("./extraRunsConcededPerTeam2016");
+const top10EconomicalBowlers2015 = require("./top10EconomicalBowlers2015");
 
-const path1 = "../public/output/matchesPerYear.json"
-const path2 = "../public/output/matchesWonPerTeam.json"
+const outputProblem1 = "../public/output/matchesPerYear.json"
+const outputProblem2 = "../public/output/matchesWonPerTeam.json"
+const outputProblem3 = "../public/output/extraRuns2016.json"
+const outputProblem4 = "../public/output/economicalBowlers2015.json"
 
 function main() {
     csv()
@@ -17,20 +21,30 @@ function main() {
                 .then((deliveries) => {
                     let result1 = matchesPlayedPerYear(matches)
                     let result2 = matchesWonPerTeamPerYear(matches)
-                    //console.log(result2)
-                    convertAndSave(result1, result2)
+                    let result3 = extraRunsConcededPerTeam2016(matches, deliveries)
+                    let result4 = top10EconomicalBowlers2015(matches, deliveries)
+                    console.log(result4)
+
+                    convertAndSave(result1, result2, result3)
                 })
         })
 }
-function convertAndSave(data1, data2) {
+
+function convertAndSave(data1, data2, data3) {
     data1 = JSON.stringify(data1)
     data2 = JSON.stringify(data2)
-    fs.writeFile(path1, data1, 'utf8', (error) => {
+    data3 = JSON.stringify(data3)
+    fs.writeFile(outputProblem1, data1, 'utf8', (error) => {
         if (error) {
             return error
         }
     })
-    fs.writeFile(path2, data2, 'utf8', (error) => {
+    fs.writeFile(outputProblem2, data2, 'utf8', (error) => {
+        if (error) {
+            return error
+        }
+    })
+    fs.writeFile(outputProblem3, data3, 'utf8', (error) => {
         if (error) {
             return error
         }
