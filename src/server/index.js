40fca96c9a71cadbb8ab47bce5db9e78ad0aaff3
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-05-27 18:38:29 +0530

    Added Spaces in json output & name change -Added the third porperty in JSON.stringify() in all writeFile methods in convertAndSave function because json output was not readable -Changed the module importing name from csv to csvToJson to follow the best practice

--- src/server/index.js
+++ src/server/index.js
@@ -1,24 +1,24 @@
-const csv = require("csvtojson")
-const fs = require('fs')
+const csvToJson = require("csvtojson");
+const fs = require('fs');
 
-const matches = "src/data/matches.csv"
-const deliveries = "src/data/deliveries.csv"
+const matches = "src/data/matches.csv";
+const deliveries = "src/data/deliveries.csv";
 
-const matchesPlayedPerYear = require("./matchesPlayedPerYear")
-const matchesWonPerTeamPerYear = require("./matchesWonPerTeamPerYear")
-const extraRunsConcededPerTeam2016 = require("./extraRunsConcededPerTeam2016")
-const top10EconomicalBowlers2015 = require("./top10EconomicalBowlers2015")
-const teamsWonTossMatch = require("./teamsWonTossMatch")
-const playerOfMatch = require("./playerOfMatch")
-const batsmanStrikeRate = require("./batsmanStrikeRate")
-const dismissedPlayer = require("./dismissedPlayer")
-const superOver = require("./superOver")
+const matchesPlayedPerYear = require("./matchesPlayedPerYear");
+const matchesWonPerTeamPerYear = require("./matchesWonPerTeamPerYear");
+const extraRunsConcededPerTeam2016 = require("./extraRunsConcededPerTeam2016");
+const top10EconomicalBowlers2015 = require("./top10EconomicalBowlers2015");
+const teamsWonTossMatch = require("./teamsWonTossMatch");
+const playerOfMatch = require("./playerOfMatch");
+const batsmanStrikeRate = require("./batsmanStrikeRate");
+const dismissedPlayer = require("./dismissedPlayer");
+const superOver = require("./superOver");
 
 function main() {
-    csv()
+    csvToJson()
         .fromFile(matches)
         .then((matches) => {
-            csv()
+            csvToJson()
                 .fromFile(deliveries)
                 .then((deliveries) => {
 
@@ -32,85 +32,87 @@ function main() {
                         strikeRate: batsmanStrikeRate(matches, deliveries),
                         playerDismissed: dismissedPlayer(deliveries),
                         superOverEconomy: superOver(deliveries)
-                    }
+                    };
 
-                    convertAndSave(calculatedValues)
-                })
-        })
+                    convertAndSave(calculatedValues);
+                });
+        });
 }
 
 function convertAndSave({ matchPlayedPerYear, matchWonPerTeam, extraRuns, tenEconomicPlayer, wonTossAndMatch, winnerOfPlayerOfMatch, strikeRate, playerDismissed, superOverEconomy }) {
 
-    fs.writeFile("src/public/output/matchesPerYear.json", JSON.stringify(matchPlayedPerYear), 'utf8', (error) => {
+    fs.writeFile("src/public/output/matchesPerYear.json", JSON.stringify(matchPlayedPerYear, null, 2), 'utf8', (error) => {
         if (error) {
-            return error
+            return error;
         } else {
-            console.log("outputfile matchesPerYear.json is created")
+            console.log("outputfile matchesPerYear.json is created");
         }
-    })
-    fs.writeFile("src/public/output/matchesWonPerTeam.json", JSON.stringify(matchWonPerTeam), 'utf8', (error) => {
-        if (error) {
-            return error
-        } else {
-            console.log("outputfile matchesWonPerTeam.json is created")
-        }
-    })
-    fs.writeFile("src/public/output/extraRuns2016.json", JSON.stringify(extraRuns), 'utf8', (error) => {
-        if (error) {
-            return error
-        } else {
-            console.log("outputfile extraRuns2016.json is created")
-        }
+    });
 
-    })
-    fs.writeFile("src/public/output/economicalBowlers2015.json", JSON.stringify(tenEconomicPlayer), 'utf8', (error) => {
+    fs.writeFile("src/public/output/matchesWonPerTeam.json", JSON.stringify(matchWonPerTeam, null, 2), 'utf8', (error) => {
         if (error) {
-            return error
+            return error;
         } else {
-            console.log("outputfile economicalBowlers2015.json is created")
+            console.log("outputfile matchesWonPerTeam.json is created");
         }
+    });
 
-    })
-    fs.writeFile("src/public/output/teamsWonTossMatch.json", JSON.stringify(wonTossAndMatch), 'utf8', (error) => {
+    fs.writeFile("src/public/output/extraRuns2016.json", JSON.stringify(extraRuns, null, 2), 'utf8', (error) => {
         if (error) {
-            return error
+            return error;
         } else {
-            console.log("outputfile teamsWonTossMatch.json is created")
+            console.log("outputfile extraRuns2016.json is created");
         }
+    });
 
-    })
-    fs.writeFile("src/public/output/playerOfMatch.json", JSON.stringify(winnerOfPlayerOfMatch), 'utf8', (error) => {
+    fs.writeFile("src/public/output/economicalBowlers2015.json", JSON.stringify(tenEconomicPlayer, null, 2), 'utf8', (error) => {
         if (error) {
-            return error
+            return error;
         } else {
-            console.log("outputfile playerOfMatch.json is created")
+            console.log("outputfile economicalBowlers2015.json is created");
         }
+    });
 
-    })
-    fs.writeFile("src/public/output/strikeRate.json", JSON.stringify(strikeRate), 'utf8', (error) => {
+    fs.writeFile("src/public/output/teamsWonTossMatch.json", JSON.stringify(wonTossAndMatch, null, 2), 'utf8', (error) => {
         if (error) {
-            return error
+            return error;
         } else {
-            console.log("outputfile strikeRate.json is created")
+            console.log("outputfile teamsWonTossMatch.json is created");
         }
+    });
 
-    })
-    fs.writeFile("src/public/output/highestDismissedPlayer.json", JSON.stringify(playerDismissed), 'utf8', (error) => {
+    fs.writeFile("src/public/output/playerOfMatch.json", JSON.stringify(winnerOfPlayerOfMatch, null, 2), 'utf8', (error) => {
         if (error) {
-            return error
+            return error;
         } else {
-            console.log("outputfile highestDismissedPlayer.json is created")
+            console.log("outputfile playerOfMatch.json is created");
         }
+    });
 
-    })
-    fs.writeFile("src/public/output/bestSuperOverBowler.json", JSON.stringify(superOverEconomy), 'utf8', (error) => {
+    fs.writeFile("src/public/output/strikeRate.json", JSON.stringify(strikeRate, null, 2), 'utf8', (error) => {
         if (error) {
-            return error
+            return error;
         } else {
-            console.log("outputfile bestSuperOverBowler.json is created")
+            console.log("outputfile strikeRate.json is created");
         }
-    })
+    });
+
+    fs.writeFile("src/public/output/highestDismissedPlayer.json", JSON.stringify(playerDismissed, null, 2), 'utf8', (error) => {
+        if (error) {
+            return error;
+        } else {
+            console.log("outputfile highestDismissedPlayer.json is created");
+        }
+    });
+
+    fs.writeFile("src/public/output/bestSuperOverBowler.json", JSON.stringify(superOverEconomy, null, 2), 'utf8', (error) => {
+        if (error) {
+            return error;
+        } else {
+            console.log("outputfile bestSuperOverBowler.json is created");
+        }
+    });
 
 }
 
-main()
+main();
